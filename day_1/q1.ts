79ce276ecd44f299eda5635174a06cc569367f85
Ezra Chai <juanzhe2@gmail.com>
2024-12-01 19:23:16 +0800

    formatted

--- day_1/q1.ts
+++ day_1/q1.ts
@@ -2,7 +2,7 @@ let text = "";
 using file = await Deno.openSync("./input.txt", { read: true });
 const fileInfo = await file.stat();
 if (fileInfo.isFile) {
-  const size =fileInfo.size;
+  const size = fileInfo.size;
   const buf = new Uint8Array(size);
   file.readSync(buf);
   text = new TextDecoder().decode(buf);
@@ -15,19 +15,18 @@ text.split("\n").forEach((line, index) => {
   const lineArr = line.split("   ");
   firstRow[index] = +lineArr[0];
   secondRow[index] = +lineArr[1];
-})
+});
 
 const firstRowSorted: Array<number> = firstRow.toSorted();
 const secondRowSorted: Array<number> = secondRow.toSorted();
 
 let sum = 0;
-for(let i = 0; i < firstRowSorted.length; i++){
-
-  if(secondRowSorted[i] > firstRowSorted[i]){
+for (let i = 0; i < firstRowSorted.length; i++) {
+  if (secondRowSorted[i] > firstRowSorted[i]) {
     sum += secondRowSorted[i] - firstRowSorted[i];
-  }else {
+  } else {
     sum += firstRowSorted[i] - secondRowSorted[i];
   }
 }
 
-console.log(sum)
+console.log(sum);
--- day_1/q2.ts
+++ day_1/q2.ts
@@ -2,8 +2,8 @@ let text = "";
 using file = await Deno.openSync("./input.txt", { read: true });
 const fileInfo = await file.stat();
 if (fileInfo.isFile) {
-  const size =fileInfo.size;
-    const buf = new Uint8Array(size);
+  const size = fileInfo.size;
+  const buf = new Uint8Array(size);
   file.readSync(buf);
   text = new TextDecoder().decode(buf);
 }
@@ -11,27 +11,27 @@ if (fileInfo.isFile) {
 const firstRow = new Array<number>();
 const secondRow = new Array<number>();
 
-text.split("\n").forEach((line,index) => {
+text.split("\n").forEach((line, index) => {
   const lineArr = line.split("   ");
-    firstRow[index] = +lineArr[0];
-    secondRow[index] = +lineArr[1];
-})
+  firstRow[index] = +lineArr[0];
+  secondRow[index] = +lineArr[1];
+});
 
 const arr = [];
-for(let i = 0; i < firstRow.length; i++){
-    let total = 0;
-    secondRow.forEach((element) => {
-        if(element == firstRow[i]){
-            total++;
-        }
-    })
-    arr[i] = [firstRow[i], total]
+for (let i = 0; i < firstRow.length; i++) {
+  let total = 0;
+  secondRow.forEach((element) => {
+    if (element == firstRow[i]) {
+      total++;
+    }
+  });
+  arr[i] = [firstRow[i], total];
 }
 
-let similarityScore  = 0;
+let similarityScore = 0;
 
-for(let i = 0; i < arr.length; i++){
-    similarityScore += arr[i][0] * arr[i][1];
+for (let i = 0; i < arr.length; i++) {
+  similarityScore += arr[i][0] * arr[i][1];
 }
 
-console.log(similarityScore)
+console.log(similarityScore);
