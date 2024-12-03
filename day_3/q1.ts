509c42cf50d187d6256946f9285117b4dad76cbc
Ezra Chai <juanzhe2@gmail.com>
2024-12-03 17:23:17 +0800

    day 3

---
+++ day_3/q1.ts
@@ -0,0 +1,20 @@
+let text = "";
+using file = await Deno.openSync("./input.txt", { read: true });
+const fileInfo = await file.stat();
+if (fileInfo.isFile) {
+  const size = fileInfo.size;
+  const buf = new Uint8Array(size);
+  file.readSync(buf);
+  text = new TextDecoder().decode(buf);
+}
+// console.log(text)
+
+let sum = 0;
+
+const regex = /mul\(([0-9999]+),([0-9999]+)\)/g;
+text.matchAll(regex)?.forEach((item) => {
+    const mul = +item[1] * +item[2];
+    sum += mul;
+})
+
+console.log(sum)
---
+++ day_3/q2.ts
@@ -0,0 +1,32 @@
+let text = "";
+using file = await Deno.openSync("./input.txt", { read: true });
+const fileInfo = await file.stat();
+if (fileInfo.isFile) {
+  const size = fileInfo.size;
+  const buf = new Uint8Array(size);
+  file.readSync(buf);
+  text = new TextDecoder().decode(buf);
+}
+// console.log(text)
+
+let sum = 0;
+
+const regex = /mul\(([0-9999]+),([0-9999]+)\)|don't\(\)|do\(\)/g;
+
+let doCondition = true;
+
+text.matchAll(regex)?.forEach((item) => {
+
+    if(item[0] === "don't()") {
+        doCondition = false;
+    }else if(item[0] === "do()"){
+        doCondition = true;
+    }else {
+       if(doCondition === true){
+        const mul = +item[1] * +item[2];
+        sum += mul;
+      }
+    }
+})
+
+console.log(sum)
