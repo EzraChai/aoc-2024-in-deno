1478a6c9759111736b1906501e4889cafaee78d8
Ezra Chai <juanzhe2@gmail.com>
2024-12-05 14:46:00 +0800

    day 5

---
+++ day_5/q1.ts
@@ -0,0 +1,64 @@
+let text = "";
+const file = await Deno.openSync("./input.txt", { read: true });
+const fileInfo = await file.stat();
+if (fileInfo.isFile) {
+  const size = fileInfo.size;
+  const buf = new Uint8Array(size);
+  file.readSync(buf);
+  text = new TextDecoder().decode(buf);
+}
+const part1 = text.split("\n\n")[0].split("\n");
+const part2 = text.split("\n\n")[1];
+
+let sum = 0;
+
+const rules = new Array<Array<number>>();
+part1.forEach((line, index) => {
+  const [a, b] = line.split("|");
+  rules[index] = [+a, +b];
+});
+// console.log(rules);
+
+part2.split("\n").forEach((line) => {
+  let isRightOrder = true;
+  const numLine = line.split(",").map((num) => +num);
+
+  for (let i = 0; i < numLine.length - 1; i++) {
+    const rulesSelectedSet = new Set<number>();
+    // console.log(numLine[i]);
+
+    rules.forEach((rule) => {
+      if (
+        numLine[i] === rule[0] &&
+        numLine.find((element, index) => element === rule[1] && index > i)
+      ) {
+        rulesSelectedSet.add(rule[1]);
+      }
+    });
+
+    // console.log(rulesSelectedSet);
+    if (rulesSelectedSet.size !== numLine.length - i - 1) {
+      isRightOrder = false;
+      break;
+    }
+
+    for (let j = i + 1; j < numLine.length; j++) {
+      if (rulesSelectedSet.has(numLine[j])) {
+        isRightOrder = true;
+      } else {
+        isRightOrder = false;
+        break;
+      }
+    }
+
+    // console.log(rulesSelectedSet);
+  }
+  if (isRightOrder) {
+    // console.log(numLine);
+    const mid = (numLine.length - 1) / 2;
+    sum += numLine[mid];
+  }
+  // console.log(numLine);
+});
+
+console.log(sum);
